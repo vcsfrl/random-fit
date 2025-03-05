67640435342f40fc0cd2fbc8e735aefe1f8d7623
Varga Csaba <vcsfrl@gmail.com>
2025-03-05 10:22:00 +0200

    Create generator package.

--- internal/generator/generator.go
+++ internal/generator/generator.go
@@ -1,11 +1,22 @@
-package internal
+package generator
 
 import (
 	"random-fit/internal/core"
 	"time"
 )
 
-func Generate(definition core.ElementDefinition) core.Element {
+type Generator struct {
+}
+
+func (g *Generator) Collection(definition core.CollectionDefinition) core.Collection {
+	return core.Collection{}
+}
+
+func (g *Generator) Set(definition core.SetDefinition) core.Set {
+	return core.Set{}
+}
+
+func (g *Generator) Element(definition core.ElementDefinition) core.Element {
 	return core.Element{
 		Identity: core.Identity{
 			ID:           "element-1",
