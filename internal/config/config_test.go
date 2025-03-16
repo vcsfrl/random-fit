9b530933e78f4069fb13d4215b0ff552dcded268
Varga Csaba <vcsfrl@gmail.com>
2025-03-16 09:01:38 +0200

    Add start builder for collection.

--- internal/config/config_test.go
+++ internal/config/config_test.go
@@ -1,16 +1,9 @@
 package config
 
 import (
-	"encoding/json"
-	"fmt"
 	"github.com/davecgh/go-spew/spew"
-	"github.com/google/uuid"
 	"github.com/stretchr/testify/suite"
-	"github.com/vcsfrl/random-fit/internal/core"
-	"go.starlark.net/starlark"
-	"log"
 	"testing"
-	"time"
 )
 
 func TestConfigSuite(t *testing.T) {
