d99e4119768acf5474da9da1cb292413daed2e59
Varga Csaba <vcsfrl@gmail.com>
2025-04-17 14:56:38 +0300

    Add generator tests.

--- internal/combination/definition_test.go
+++ internal/combination/definition_test.go
@@ -1,8 +1,10 @@
 package combination
 
 import (
+	"fmt"
 	"github.com/stretchr/testify/suite"
 	"testing"
+	"time"
 )
 
 func TestCombinationDefinitionSuite(t *testing.T) {
@@ -11,22 +13,47 @@ func TestCombinationDefinitionSuite(t *testing.T) {
 
 type CombinationDefinitionSuite struct {
 	suite.Suite
+
+	definition *StarlarkDefinition
+	id         int
+	testRand   uint
+	scriptFile string
 }
 
-func (suite *CombinationDefinitionSuite) TestNewCombinationDefinition() {
-	script := "./testdata/star_script.star"
+func (suite *CombinationDefinitionSuite) SetupTest() {
+	suite.scriptFile = "./testdata/star_script.star"
 
-	definition, err := NewCombinationDefinition(script)
+	var err error
+	suite.definition, err = NewCombinationDefinition(suite.scriptFile)
 	suite.NoError(err)
-	suite.NotNil(definition)
+	suite.NotNil(suite.definition)
 
-	suite.NotEmpty(definition.ID)
-	suite.Equal("Lotto Number Picks", definition.Name)
-	suite.Equal(script, definition.StarScript)
-	suite.Contains(definition.GoTemplate, "/*gotype:")
+	suite.id = 0
+	suite.definition.uuidFunc = func() (string, error) {
+		suite.id++
+		return fmt.Sprintf("00000000-0000-0000-0000-%012d", suite.id), nil
+	}
 
-	suite.NotNil(definition.buildFunction)
-	combinationGenerator, err := definition.Generator()
+	suite.definition.nowFunc = func() time.Time {
+		return time.Date(2021, 1, 1, 0, 0, 0, 0, time.UTC)
+	}
+
+	suite.definition.randomUintFunc = func(min uint, max uint) (uint, error) {
+		suite.testRand++
+		return suite.testRand, nil
+	}
+
+	suite.NoError(err)
+}
+
+func (suite *CombinationDefinitionSuite) TestCombinationDefinition_Generator() {
+	suite.NotEmpty(suite.definition.ID)
+	suite.Equal("Lotto Number Picks", suite.definition.Name)
+	suite.Equal(suite.scriptFile, suite.definition.StarScript)
+	suite.Contains(suite.definition.GoTemplate, "/*gotype:")
+
+	suite.NotNil(suite.definition.buildFunction)
+	combinationGenerator, err := suite.definition.Generator()
 	suite.NoError(err)
 
 	combination, err := combinationGenerator()
@@ -34,12 +61,22 @@ func (suite *CombinationDefinitionSuite) TestNewCombinationDefinition() {
 	suite.NotNil(combination)
 
 	suite.NotEmpty(combination.UUID)
-	suite.Equal(definition.ID, combination.DefinitionID)
-	suite.Equal(definition.Name, combination.Name)
-	suite.Equal(definition.GoTemplate, combination.GoTemplate)
+	suite.Equal(suite.definition.ID, combination.DefinitionID)
+	suite.Equal(suite.definition.Name, combination.Name)
+	suite.Equal(suite.definition.GoTemplate, combination.GoTemplate)
 	suite.NotEmpty(combination.Data)
-	suite.NotEmpty(
-		"User 1 Monthly Lotto Number picks",
-		combination.Data.(map[string]any)["Collections"].([]any)[0].(map[string]any)["Metadata"].(map[string]any)["Description"],
-	)
+
+	stringData := fmt.Sprintf("%+v", combination.Data)
+
+	suite.Contains(stringData, "Name:6/49 and Lucky Number")
+	suite.Contains(stringData, "User 1 Monthly Lotto Number picks")
+	suite.Contains(stringData, "User 2 Monthly Lotto Number picks")
+	suite.Contains(stringData, "[1 2 3 4 5 6]")
+	suite.Contains(stringData, "[36 37 38 39 40 41]")
+	suite.Contains(stringData, "collection_00000000-0000-0000-0000-000000000001")
+	suite.Contains(stringData, "element_00000000-0000-0000-0000-000000000021")
+	suite.Contains(stringData, "Lucky Number")
+	suite.Contains(stringData, "Values:4200")
+
+	suite.Equal(4205, len(stringData))
 }
