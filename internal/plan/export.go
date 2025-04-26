6f14b62ed963a6e58985cb46158df7addeacb3e7
Varga Csaba <vcsfrl@gmail.com>
2025-04-26 08:55:03 +0300

    Generate files.

--- internal/plan/export.go
+++ internal/plan/export.go
@@ -2,10 +2,13 @@ package plan
 
 import (
 	"fmt"
+	"github.com/vcsfrl/random-fit/internal/combination"
 	"os"
 	"path/filepath"
 	"strings"
 )
+
+var ErrExport = fmt.Errorf("error exporting plan")
 
 type Exporter struct {
 	OutputDir string
@@ -20,15 +23,7 @@ func NewExporter(outputDir string) *Exporter {
 func (e *Exporter) Export(plan *Plan) error {
 	for userID, groups := range plan.UserGroups {
 		for _, group := range groups {
-			// Create a folder for the group
-			groupFolder := filepath.Join(
-				e.OutputDir,
-				userID,
-				fmt.Sprintf("%s_%s", plan.CreatedAt.Format("2006-01-02-1504"), group.ContainerName),
-				group.Details,
-			)
-			groupFolder = strings.ReplaceAll(groupFolder, " ", "_")
-			err := os.MkdirAll(groupFolder, 0755)
+			groupFolder, err := e.createGroupFolder(plan, group, userID)
 			if err != nil {
 				return err
 			}
@@ -36,22 +31,10 @@ func (e *Exporter) Export(plan *Plan) error {
 			// Create a file for each combination by type
 			for _, combination := range group.Combinations {
 				for _, data := range combination.Data {
-					// create file
-					fileName := fmt.Sprintf("%s.%s", combination.Details, data.Extension)
-					fileName = strings.ReplaceAll(fileName, " ", "_")
-					filePath := filepath.Join(groupFolder, fileName)
-					file, err := os.Create(filePath)
-					if err != nil {
-						return err
+					err2 := e.saveToFile(combination, data, groupFolder)
+					if err2 != nil {
+						return err2
 					}
-
-					_, err = file.Write(data.Data.Bytes())
-					if err != nil {
-						return err
-
-					}
-
-					err = file.Close()
 				}
 
 			}
@@ -59,4 +42,33 @@ func (e *Exporter) Export(plan *Plan) error {
 		}
 	}
 	return nil
+}
+
+func (e *Exporter) saveToFile(combination *combination.Combination, data *combination.Data, groupFolder string) error {
+	fileName := fmt.Sprintf("%s.%s", combination.Details, data.Extension)
+	filePath := filepath.Join(groupFolder, strings.ReplaceAll(fileName, " ", "_"))
+	err := os.WriteFile(filePath, data.Data.Bytes(), 0666)
+	if err != nil {
+		return fmt.Errorf("%w: error writing file: %s", ErrExport, err)
+	}
+
+	return nil
+}
+
+func (e *Exporter) createGroupFolder(plan *Plan, group *Group, userID string) (string, error) {
+	// Create a folder for the group
+	groupFolder := filepath.Join(
+		e.OutputDir,
+		userID,
+		fmt.Sprintf("%s_%s", plan.CreatedAt.Format("2006-01-02-1504"), group.ContainerName),
+		group.Details,
+	)
+
+	groupFolder = strings.ReplaceAll(groupFolder, " ", "_")
+	err := os.MkdirAll(groupFolder, 0755)
+	if err != nil {
+		return "", fmt.Errorf("%w: error creating group folder: %s", ErrExport, err)
+	}
+
+	return groupFolder, err
 }
