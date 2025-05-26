c97d8feb4bd5d66d3ca1580e1f8967c271e27a6c
Varga Csaba <vcsfrl@gmail.com>
2025-05-26 09:57:57 +0300

    Fix linting issues. Introduce translations.

--- cmd/translations/translations.go
+++ cmd/translations/translations.go
@@ -1,3 +1,4 @@
 package translations
 
 //go:generate gotext -srclang=en-GB update -out=catalog.go -lang=en-US github.com/vcsfrl/random-fit/cmd
+// https://www.alexedwards.net/blog/i18n-managing-translations
